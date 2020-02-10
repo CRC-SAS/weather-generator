655168069677532e58f83510726a5e2591337b66
Daniel Bonhaure <danielbonhaure@gmail.com>
2020-02-10 23:09:58 +0000

    Added autorun from yaml file

--- R/autorun.R
+++ R/autorun.R
@@ -25,7 +25,6 @@ autorun_from_yaml <- function(params_yaml_filename = NULL) {
     if (use_package_provided_files) {
         params$fit$climate  <- system.file(glue::glue("autorun/{params$model}"), "climate.csv",  package = "gamwgen")
         params$fit$stations <- system.file(glue::glue("autorun/{params$model}"), "stations.csv", package = "gamwgen")
-        params$fit$stations <- system.file(glue::glue("autorun/{params$model}"), "stations.csv", package = "gamwgen")
         params$fit$seasonal_covariates  <- system.file(glue::glue("autorun/{params$model}"), "seasonal_covariates_fit.csv", package = "gamwgen")
         params$sim$simulation_locations <- system.file(glue::glue("autorun/{params$model}"), "simulation_locations.csv",    package = "gamwgen")
         params$sim$seasonal_covariates  <- system.file(glue::glue("autorun/{params$model}"), "seasonal_covariates_sim.csv", package = "gamwgen")
