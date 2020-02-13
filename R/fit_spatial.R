a94c8a847e9225207735cc10bed7b5a21d0a8466
Daniel Bonhaure <danielbonhaure@gmail.com>
2020-02-13 12:21:43 -0300

    Improvement to suppress some warnings

--- R/fit_spatial.R
+++ R/fit_spatial.R
@@ -251,7 +251,9 @@ spatial_calibrate <- function(climate, stations, seasonal_covariates = NULL,
     prcp_occ_fit_noNA_cols <- c('prcp_occ', 'prcp_occ_prev', 'doy', 'time', 'row_num')
     if (!is.null(seasonal_covariates))
         prcp_occ_fit_noNA_cols <- append(prcp_occ_fit_noNA_cols, c('ST1', 'ST2', 'ST3', 'ST4'))
-    prcp_occ_indexes <- models_data %>% tidyr::drop_na(prcp_occ_fit_noNA_cols) %>% dplyr::pull(row_num)
+    prcp_occ_indexes <- station_climate %>%
+        tidyr::drop_na(tidyselect::all_of(prcp_occ_fit_noNA_cols)) %>%
+        dplyr::pull(row_num)
 
     # Create formula
     prcp_occ_fm <- prcp_occ ~ s(time, bs = "gp", k = 1000) +
@@ -310,7 +312,9 @@ spatial_calibrate <- function(climate, stations, seasonal_covariates = NULL,
     prcp_amt_fit_noNA_cols <- c('prcp_amt', 'prcp_occ_prev', 'doy', 'time', 'row_num')
     if (!is.null(seasonal_covariates))
         prcp_amt_fit_noNA_cols <- append(prcp_amt_fit_noNA_cols, c('ST1', 'ST2', 'ST3', 'ST4'))
-    gamma_indexes <- models_data %>% tidyr::drop_na(prcp_amt_fit_noNA_cols) %>% dplyr::pull(row_num)
+    gamma_indexes <- station_climate %>%
+        tidyr::drop_na(tidyselect::all_of(prcp_amt_fit_noNA_cols)) %>%
+        dplyr::pull(row_num)
 
     # Create formula
     prcp_amt_fm <- prcp_amt ~ te(tipo_dia_prev, longitude, latitude, d = c(1, 2), bs = c('re', 'tp'), k = length(unique_stations))
@@ -366,7 +370,9 @@ spatial_calibrate <- function(climate, stations, seasonal_covariates = NULL,
     tmax_fit_noNA_cols <- c('tmax', 'tmax_prev', 'tmin_prev', 'prcp_occ', 'prcp_occ_prev', 'row_num')
     if (!is.null(seasonal_covariates))
         tmax_fit_noNA_cols <- append(tmax_fit_noNA_cols, c('seasonal_tmax', 'seasonal_tmin'))
-    tmax_indexes <- models_data %>% tidyr::drop_na(tmax_fit_noNA_cols) %>% dplyr::pull(row_num)
+    tmax_indexes <- station_climate %>%
+        tidyr::drop_na(tidyselect::all_of(tmax_fit_noNA_cols)) %>%
+        dplyr::pull(row_num)
 
     # Create formula
     tmax_fm <- tmax ~ s(time, bs = 'gp', k = 1000) +
@@ -427,7 +433,9 @@ spatial_calibrate <- function(climate, stations, seasonal_covariates = NULL,
     tmin_fit_noNA_cols <- c('tmin', 'tmax_prev', 'tmin_prev', 'prcp_occ', 'prcp_occ_prev', 'row_num')
     if (!is.null(seasonal_covariates))
         tmin_fit_noNA_cols <- append(tmin_fit_noNA_cols, c('seasonal_tmax', 'seasonal_tmin'))
-    tmin_indexes <- models_data %>% tidyr::drop_na(tmin_fit_noNA_cols) %>% dplyr::pull(row_num)
+    tmin_indexes <- station_climate %>%
+        tidyr::drop_na(tidyselect::all_of(tmin_fit_noNA_cols)) %>%
+        dplyr::pull(row_num)
 
     # Create formula
     tmin_fm <- tmin ~ s(time, bs = 'gp', k = 1000) +
